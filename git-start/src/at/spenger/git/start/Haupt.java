cd56655d5a8c6586efd7200de4b7739b0de74375
htlspenger2013 <htl-spenger-2013@gmx.at>
2014-09-07 17:29:32 +0200

    Update Haupt.java

--- git-start/src/at/spenger/git/start/Haupt.java
+++ git-start/src/at/spenger/git/start/Haupt.java
@@ -4,7 +4,7 @@ public class Haupt {
 
 	public static void main(String[] args) {
 		System.out.println("Hallo Welt!");
-		System.out.println("Ciao Mondo");
+		System.out.println("Ciao Mondo!");
 		System.out.println("Hello World!");
 
 	}
