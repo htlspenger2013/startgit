0bc72385efe3c06209cc51858d2b16b3f2f04e24
htl-tom <htl-spenger-2013@gmx.at>
2014-09-13 11:20:14 +0200

    Mit date

--- git-start/src/at/spenger/git/start/Haupt.java
+++ git-start/src/at/spenger/git/start/Haupt.java
@@ -1,5 +1,13 @@
 package at.spenger.git.start;
 
+import java.time.Instant;
+import java.time.LocalDate;
+import java.time.Period;
+import java.time.ZoneId;
+import java.time.format.DateTimeFormatter;
+
+
+
 public class Haupt {
 
 	public static void main(String[] args) {
@@ -7,6 +15,24 @@ public class Haupt {
 		System.out.println("Ciao Mondo!");
 		System.out.println("Hello World!");
 
+		
+		// ms seit 1.1.1970
+		long timeStamp = Instant.now().toEpochMilli();
+		
+		String pattern = "yyyy.MM.dd";
+		pattern = "yyyy-MM-dd";
+		// Coordinated Universal Time (UTC)
+		// based on International Atomic Time (TAI), introduced 1972
+		// With leap seconds --> 2012 TAI has been 35 s ahead of UTC
+		DateTimeFormatter dtf = DateTimeFormatter.ofPattern(pattern).withZone(ZoneId.of("UTC"));
+		
+		System.out.println(dtf.format(Instant.now()));
+		
+		
+		Period p = Period.between(LocalDate.parse("1968-09-27"), LocalDate.now());
+		System.out.printf("Jahre: %d ", p.getYears());
+		System.out.printf("Monate: %d Tage: %d", p.getMonths(), p.getDays());
+		
 	}
 
 }
